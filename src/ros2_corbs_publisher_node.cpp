f31d2b74984567487b362a1e58aeadc18d2e025b
Daniel Hettegger <daniel.hettegger@tum.de>
2020-06-16 21:37:02 +0200

    Added image publishers and timer

--- src/ros2_corbs_publisher_node.cpp
+++ src/ros2_corbs_publisher_node.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
 using namespace std::chrono_literals;
@@ -13,10 +14,26 @@ class CoRBSPublisher : public rclcpp::Node
 {
   public:
     CoRBSPublisher()
-    : Node("ros2_corbs_publisher_node"), count_(0)
+    : Node("ros2_corbs_publisher_node")
     {
+      _depth_image_publisher = this->create_publisher<sensor_msgs::msg::Image>("depth_image", 10);
+      _color_image_publisher = this->create_publisher<sensor_msgs::msg::Image>("color_image", 10);
+      _timer = this->create_wall_timer(
+        33ms, std::bind(&CoRBSPublisher::timer_callback, this));
     }
 
+  private:
+    void timer_callback()
+    {
+      auto depth_image_message = sensor_msgs::msg::Image();
+      auto color_image_message = sensor_msgs::msg::Image();
+      _depth_image_publisher->publish(depth_image_message);
+      _color_image_publisher->publish(color_image_message);
+    }
+    
+    rclcpp::TimerBase::SharedPtr _timer;
+    rclcpp::Publisher<std_msgs::msg::Image>::SharedPtr _depth_image_publisher;
+    rclcpp::Publisher<std_msgs::msg::Image>::SharedPtr _color_image_publisher;
   };
 
   int main(int argc, char * argv[])
