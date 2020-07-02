e189aaccc9b95b0830e0ef62978c305caf621398
DanielHettegger <daniel.hettegger@tum.de>
2020-07-02 11:39:38 +0200

    Added trajectory publishing

--- src/ros2_corbs_publisher_node.cpp
+++ src/ros2_corbs_publisher_node.cpp
@@ -13,6 +13,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 
 using namespace std::chrono_literals;
 
@@ -24,23 +25,32 @@ class CoRBSPublisher : public rclcpp::Node
     {
       _depth_image_publisher = this->create_publisher<sensor_msgs::msg::Image>("depth_image", 10);
       _color_image_publisher = this->create_publisher<sensor_msgs::msg::Image>("color_image", 10);
+      _trajectory_publisher = this->create_publisher<geometry_msgs::msg::PoseStamped>("camera_pose", 10);
 
       _frame_id = this->declare_parameter("frame_id", "camera_frame");
       _data_path = this->declare_parameter("data_path", "./");
+      _trajectory_path = this->declare_parameter("trajectory_path", "./");
 
      _depth_filenames_stream = std::ifstream{_data_path + "depth.txt"};
      _color_filenames_stream = std::ifstream{_data_path + "rgb.txt"};
+     _trajectory_stream = std::ifstream{_trajectory_path + "groundtruth.txt"};
 
       if (!_depth_filenames_stream.is_open() || !_color_filenames_stream.is_open())
         throw std::runtime_error{"Data files could not be read.."};
 
-      // Skip ahead first 8 elements
+      if (!_trajectory_stream.is_open())
+        throw std::runtime_error{"Trajectory file could not be read.."};
+
       std::string s;
       for(int i = 0; i < 8; i++){
         _depth_filenames_stream >> s;
         _color_filenames_stream >> s;
       }
 
+      for(int i = 0; i < 16; i++){
+        _trajectory_stream >> s;
+        RCLCPP_INFO(this->get_logger(), s);
+      }
       _timer = this->create_wall_timer(
         33ms, std::bind(&CoRBSPublisher::timer_callback, this));
     }
@@ -49,20 +59,33 @@ class CoRBSPublisher : public rclcpp::Node
     rclcpp::TimerBase::SharedPtr _timer;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _depth_image_publisher;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr _color_image_publisher;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr _trajectory_publisher;
 
     std::string _frame_id; 
     std::string _data_path;
+    std::string _trajectory_path;
 
     std::ifstream _depth_filenames_stream;
     std::ifstream _color_filenames_stream;
+    std::ifstream _trajectory_stream;
+
+    /*bool _once = false;
+    std::string depth_filename, color_filename, depth_time_stamp, color_time_stamp;*/
+
 
     void timer_callback()
     {
+      //if(!_once){
       std::string depth_filename, color_filename, depth_time_stamp, color_time_stamp;
+      std::string tx, ty, tz, qx, qy, qz, qw, pose_timestamp;
       std::stringstream debug_ss;
 
       _depth_filenames_stream >> depth_time_stamp >> depth_filename;
       _color_filenames_stream >> color_time_stamp >> color_filename;
+
+      do {
+        _trajectory_stream >> pose_timestamp >> tx >> ty >> tz >> qx >> qy >> qz >> qw;
+      } while(stod(depth_time_stamp) > stod(pose_timestamp));
 
       if(depth_filename.empty()){
         RCLCPP_INFO(this->get_logger(), "END OF STREAM REACHED");
@@ -75,11 +98,16 @@ class CoRBSPublisher : public rclcpp::Node
 
       debug_ss << "Reading images:" << std::endl; 
       debug_ss << depth_time_stamp << ": " << depth_filename << std::endl;
-      debug_ss << color_time_stamp << ": " << color_filename;
+      debug_ss << color_time_stamp << ": " << color_filename << std::endl;
+      debug_ss << pose_timestamp << ": " << tx << " " << ty << " " << tz << " " << qx << " " << qy << " " << qz << " " << qw << std::endl;
+      //debug_ss << (stod(depth_time_stamp) > stod(pose_timestamp) ? "depth > pose" : "depth < pose") << " " << stod(depth_time_stamp) - stod(pose_timestamp);
       RCLCPP_DEBUG(this->get_logger(), debug_ss.str());
+
+      //_once = true;}
 
       cv::Mat_<float> depth_map;
       cv::Mat_<cv::Vec3b> color_map;
+      //cv::Mat_<cv::Vec3b> color_map_resized;
 
       depth_map = cv::imread(depth_filename, CV_LOAD_IMAGE_ANYDEPTH);
       if(depth_map.empty()){
@@ -87,9 +115,15 @@ class CoRBSPublisher : public rclcpp::Node
         return;
       }
 
-      depth_map *= 0.0002;
+      depth_map *= 0.0002 * 1000;
       
       color_map = cv::imread(color_filename);
+
+      //cv::resize(color_map, color_map_resized, cv::Size(depth_map.cols ,depth_map.rows), 0, 0, CV_INTER_LINEAR);
+
+      /*cv::imshow("depth_map", depth_map);
+      cv::imshow("color_map", color_map);
+      cv::waitKey(1);*/
 
       auto depth_image_message = std::make_unique<sensor_msgs::msg::Image>();
       auto color_image_message = std::make_unique<sensor_msgs::msg::Image>();
@@ -102,6 +136,22 @@ class CoRBSPublisher : public rclcpp::Node
       
       _depth_image_publisher->publish(std::move(depth_image_message));
       _color_image_publisher->publish(std::move(color_image_message));
+
+
+      // Publish current pose
+      auto pose_message = std::make_unique<geometry_msgs::msg::PoseStamped>();
+      add_timestamp_string_to_message(pose_timestamp, *pose_message);
+
+      pose_message->pose.position.x = stod(tx);
+      pose_message->pose.position.y = stod(ty);
+      pose_message->pose.position.z = stod(tz);
+      pose_message->pose.orientation.x = stod(qx);
+      pose_message->pose.orientation.y = stod(qy);
+      pose_message->pose.orientation.z = stod(qz);
+      pose_message->pose.orientation.w = stod(qw);
+
+      _trajectory_publisher->publish(std::move(pose_message));
+
     }
 
     //From ROS2 image_tools cam2image [https://github.com/ros2/demos/blob/master/image_tools/src/cam2image.cpp]
@@ -146,6 +196,15 @@ class CoRBSPublisher : public rclcpp::Node
       msg.header.stamp.sec = std::stoi(timestamp_string.substr(0,split));
       msg.header.stamp.nanosec = std::stoi(timestamp_string.substr(split+1,timestamp_string.length())) * 1000;
     }
+
+    void add_timestamp_string_to_message(
+      std::string timestamp_string, geometry_msgs::msg::PoseStamped& msg)
+    {
+      auto split = timestamp_string.find('.');
+      msg.header.stamp.sec = std::stoi(timestamp_string.substr(0,split));
+      msg.header.stamp.nanosec = std::stoi(timestamp_string.substr(split+1,timestamp_string.length())) * 1000;
+    }
+
   };
 
   int main(int argc, char * argv[])
